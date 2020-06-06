2aee0e09dbf9dc534ed025994ebd6d01a60bc30e
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-06-07 01:20:43 +0530

    update js

--- static/s2.js
+++ static/s2.js
@@ -1,9 +1,41 @@
-var getData = $.get('/df2');
-getData.done(function(df2){
-for(var i=0;i<692;i++){
-    document.getElementById("sel1").innerHTML += '<option value="'+df2.district[i]+'">'+df2.district[i]+'</option>';
+  var getstate = $.get('/state');
+getstate.done(function(state){
+for(var i=0;i<34;i++){
+    document.getElementById("sel2").innerHTML += '<option value="'+state.state[i]+'">'+state.state[i]+'</option>';
 }
 });
+
+$(document).ready(function() {
+
+  $("#sel2").change(function() {
+
+    var datastate = $(this).val() ;
+    document.getElementById("sel1").innerHTML="";
+    console.log(datastate);
+        $.ajax({
+            type: "POST",
+            url: "/df2",
+            data: { st : datastate } 
+        }).done(function(df2){
+          for(var i=0;i<df2.district.length;i++){
+            document.getElementById("sel1").innerHTML += '<option value="'+df2.district[i]+'">'+df2.district[i]+'</option>';
+                            }
+        });
+
+  });
+
+});
+
+function fung(){
+    var ds=$("#sel1").val();
+    $.ajax({
+            type: "POST",
+            url: "/district",
+            data: { district : ds } 
+        }).done(function(df2){
+        console.log(ds);
+document.getElementById("g").innerHTML = "";
+document.getElementById("g").innerHTML ="<canvas id='disj' ></canvas>";
 var getData2 = $.get('/disj');
 getData2.done(function(disj){
     var ctx = document.getElementById('disj').getContext('2d');
@@ -14,7 +46,7 @@ getData2.done(function(disj){
         data: {
             labels: ["Density","Workplaces","Residential","Retail","Grocery","Parks","Stations","Air quality","Water accessibility","Thermal anomalies","Confirmed","Active"],
             datasets: [{
-                label: 'District',
+                label: ds,
                 fill: true,
                 borderWidth: 1,
                 borderColor: 'rgb(255, 0, 30)',
@@ -48,6 +80,10 @@ getData2.done(function(disj){
         }
     });
 });
+        });
+
+
+}
 
 var getData3 = $.get('/getChartData');
 getData3.done(function(data){
